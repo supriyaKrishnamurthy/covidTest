bd035b3156661e4111636ee1ca15c46912fa53f4
Supriya <supriyakm21@gmail.com>
2020-04-06 22:32:47 +0530

    Removed unnecessary components like display-stat,graph,map

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,12 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CountryListComponent } from './country-list/country-list.component';
-import { DisplayStatComponent } from './display-stat/display-stat.component';
-import { MapComponent } from './map/map.component';
-import { GraphComponent } from './graph/graph.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    CountryListComponent,
-    DisplayStatComponent,
-    MapComponent,
-    GraphComponent
+    CountryListComponent
   ],
   imports: [
     BrowserModule,
