25ae75b93fe6732aa8c4a2a200cf4dc5be5d13cc
Supriya <supriyakm21@gmail.com>
2020-04-12 12:52:20 +0530

    Refined the search bar funtionality,added clear button to clear the selection and display global values,iterated through json file to get global values for graph,made the graph creation funtion generic,added banner div in html

--- src/app/country-list/country-list.component.ts
+++ src/app/country-list/country-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import covid from '../../assets/COVID.json';
 import Chart from 'chart.js';
+import { analyzeAndValidateNgModules } from '@angular/compiler';
 
 @Component({
   selector: 'app-country-list',
@@ -16,11 +17,16 @@ export class CountryListComponent implements OnInit {
   confirmed:number;
   death:number;
   recovered:number;
-  globalConfirmed:number=0;
-  globalDeath:number=0;
-  globalRecovered:number=0;
+  globalTotalConfirmed:number=0;
+  globalTotalDeath:number=0;
+  globalTotalRecovered:number=0;
   lastRow:number;
   chart:[];
+  clearTheSelection:boolean=false;
+  globalConfirmedList:Array<number>=[];
+  globalDeathList:Array<number>=[];
+  globalRecoverdList:Array<number>=[];
+  dateList:[];
 
 
   constructor() { }
@@ -31,42 +37,90 @@ export class CountryListComponent implements OnInit {
 
     Object.keys(this.covidMainData).forEach(countryName=>{
       this.lastRow=this.covidMainData[countryName].length-1;
-      this.globalConfirmed+=this.covidMainData[countryName][this.lastRow].confirmed;
-      this.globalDeath+=this.covidMainData[countryName][this.lastRow].deaths;
-      this.globalRecovered+=this.covidMainData[countryName][this.lastRow].recovered;
+      this.globalTotalConfirmed+=this.covidMainData[countryName][this.lastRow].confirmed;
+      this.globalTotalDeath+=this.covidMainData[countryName][this.lastRow].deaths;
+      this.globalTotalRecovered+=this.covidMainData[countryName][this.lastRow].recovered;
     })
   }
 
   printCountry(country){
+    this.clearTheSelection=false;
     this.clickedCountry=country.key;
     this.confirmed=country.value[country.value.length-1].confirmed;
     this.death=country.value[country.value.length-1].deaths;
     this.recovered=country.value[country.value.length-1].recovered;
 
     
-    let dateList=country.value.map(country=>country.date);
+    this.dateList=country.value.map(country=>country.date);
     let confirmedList=country.value.map(country=>country.confirmed);
     let deathList=country.value.map(country=>country.deaths);
     let recoveredList=country.value.map(country=>country.recovered);
+    
+    this.displayChart(this.dateList,confirmedList,deathList,recoveredList);
+     
+  }
 
-    this.chart=new Chart('canvas',{
+  searchByCountry(){
+    this.covidData={};
+    Object.keys(this.covidMainData).forEach(countryName=>{
+      if(countryName.toLowerCase().includes(this.searchInput.toLowerCase())){
+        this.covidData[countryName]=this.covidMainData[countryName]
+      }
+    })
+  }
+
+  clearSelection(){
+    this.clearTheSelection=true;
+    this.searchInput='';
+    this.covidData=this.covidMainData;
+
+  
+    let totalDays=this.covidMainData.Afghanistan.length-1 /**Check how to generalize this */
+   /**for each date sumup the values of each country for that date**/
+    for(let i=0;i<=totalDays;i++){
+      let globalConfirmedByDate=0;
+      let globalDeathByDate=0;
+      let globalRecoveredByDate=0
+        Object.keys(this.covidMainData).forEach(countryName=>{
+          /**for given day sumup the values of each country **/
+          globalConfirmedByDate+=this.covidMainData[countryName][i].confirmed;
+          globalDeathByDate+=this.covidMainData[countryName][i].deaths;
+          globalRecoveredByDate+=this.covidMainData[countryName][i].recovered;
+
+        })
+      /**Push data to array for each date summed up values **/
+
+        this.globalConfirmedList.push(globalConfirmedByDate);
+        this.globalDeathList.push(globalDeathByDate);
+        this.globalRecoverdList.push(globalRecoveredByDate);
+   }
+
+   console.log(this.globalConfirmedList,this.globalDeathList,this.globalRecoverdList);
+   this.displayChart(this.dateList,this.globalConfirmedList,this.globalDeathList,this.globalRecoverdList);
+
+
+  }
+
+  displayChart(dates,confirms,deaths,recovers){
+
+   this.chart=new Chart('canvas',{
       type: 'line',
       data: {
-        labels:dateList,
+        labels:dates,
         datasets: [{ 
-            data: confirmedList,
+            data: confirms,
             label: "confirmed",
             borderColor: "rgb(235, 213, 15)",
             fill: false
           }, 
            { 
-            data:deathList,
+            data:deaths,
             label: "dead",
             borderColor: "rgba(255, 136, 0, 0.795)",
             fill: false
           },
           { 
-            data: recoveredList,
+            data: recovers,
             label: "Recovered",
             borderColor: "rgb(49, 190, 49)",
             fill: false
@@ -78,15 +132,6 @@ export class CountryListComponent implements OnInit {
           display: true,
           text: 'Corona spread analysis'
         }
-      }
-    }) 
-  }
-
-  searchByCountry(){
-    this.covidData={};
-    Object.keys(this.covidMainData).forEach(countryName=>{
-      if(countryName.toLowerCase().includes(this.searchInput.toLowerCase())){
-        this.covidData[countryName]=this.covidMainData[countryName]
       }
     })
   }
